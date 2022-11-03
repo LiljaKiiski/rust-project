f4443987606b1535c67f5ff2eaa75497302ee1da
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-11-02 20:25:42 -0700

    slow and steady

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
-fn main() {
-    println!("Hello, world!");
-    let num = gcd(5, 10);
+use std::str::FromStr;
+use std::env;
 
-    println!("{}", num);
+fn main() {
+    let mut numbers = Vec::new();
+
+    for arg in env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing arg"));
+    }
+
+    if numbers.len() == 0 {
+        eprintln!("Usage: gcd NUMBER ...");
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+
+    for m in &numbers[1..]{
+        d = gcd(d, *m);
+    }
+
+    println!("GCD of {:?} is {}", numbers, d);
 }
 
 //u64 = unsigned 64 bit integer
@@ -19,5 +36,13 @@ fn gcd(mut n: u64, mut m: u64)-> u64{
         }
         m = m % n;
     }
-    n
+    n //return
+    //can also return with semicolon!
+}
+
+#[test]
+fn test_gcd(){
+    assert_eq!(gcd(14, 15), 1);
+
+    assert_eq!(gcd(2*3*5*11*17, 3*7*11*13*19), 3*11);
 }
---
+++ target/debug/deps/rust_project-e3b56b5b6adb734e.d
@@ -0,0 +1,5 @@
+/home/lilja/rust-project/target/debug/deps/rust_project-e3b56b5b6adb734e: src/main.rs
+
+/home/lilja/rust-project/target/debug/deps/rust_project-e3b56b5b6adb734e.d: src/main.rs
+
+src/main.rs:
